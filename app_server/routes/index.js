aeb0ac16dfc558e8dc02e65de00a5025864e809d
Roberto AF <mr.amparan@gmail.com>
2017-02-10 13:19:18 -0800

    Second

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var ctrlMain = require('../controllers/main'); //3.4
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
+// var homepageController = function (req, res) {
+// 	res.render('index', {title: 'Express'});
+// }
+// router.get('/', homepageController);
+router.get('/', ctrlMain.index);
+ 
 module.exports = router;
