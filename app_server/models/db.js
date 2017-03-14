a7da939dcbc288b9b1b661e287fc8d5a80e7f3e9
ideheer <ian.deheer@gmail.com>
2017-03-14 05:58:50 +0000

    added mongoose connection events

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,3 +1,20 @@
 //Require mongoos
 var mongoose = require( 'mongoose' );
 
+var dbURI = 'mongodb://localhost/UniversalStudentCollective';
+mongoose.connect(dbURI);
+
+//Monitor for successful connection through Mongoose
+mongoose.connection.on('connected', function() {
+    console.log('Mongoose connected to ' + dbURI);
+});
+
+//Check for connection error
+mongoose.connection.on('error', function (err) {
+    console.log('Mongoose connection error: ' + err);
+});
+
+//Check for disconnection event
+mongoose.connection.on('disconnected', function() {
+    console.log('Mongoose disconnected');
+});
