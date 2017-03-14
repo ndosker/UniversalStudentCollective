b4e442f88d3dfda83eeec04bc5101fd3d2face8a
ideheer <ian.deheer@gmail.com>
2017-03-14 06:26:19 +0000

    captured process termination in database connection file

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -16,5 +16,43 @@ mongoose.connection.on('error', function (err) {
 
 //Check for disconnection event
 mongoose.connection.on('disconnected', function() {
-    console.log('Mongoose disconnected');
+    console.log('Mongoose done got disconnected');
 });
+
+//gracefulShutdown accepts a message and callback function
+var gracefulShutdown = function (msg, callback) {
+    //close mongoose connection, pass through an anonymous function to run when closed
+    mongoose.connection.close(function () {
+        console.log('Mongoose disconnected through ' + msg);  // output message and callback when Mongoose connection is closed
+        callback();
+    
+    });
+};
+
+//Listen for SIGUSR2, which is what nodemon uses
+process.once('SIGUSR2', function () {
+    //Send message to gracefulShutdown and callback to kill process, emitting SIGUSR2 again
+    gracefulShutdown('nodemon restart', function () {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+
+//Listen for SIGINT emitted on app termination
+process.on('SIGINT', function () {
+    //Send message to gracefulShutdown and callback to exit Node process
+    gracefulShutdown('app termination... you killed it.', function () {
+        process.exit(0);
+    });
+});
+
+//Listen for SIGTERM emitted when Heroku shuts down process
+process.on('SIGTERM', function () {
+    //Send message to gracefulShutdown and callabck to exit Node process
+    gracefulShutdown('Heroku app shutdown', function () {
+        process.exit(0);
+    });
+});
+
+
+    
+    
