a9935afc10fe68236553c36f99834eb52ea2ba9c
ideheer <ian.deheer@gmail.com>
2017-03-16 07:32:52 +0000

    added Ashley's schema

--- app_server/models/service_providers.js
+++ app_server/models/service_providers.js
@@ -1,2 +1,23 @@
-var mongoose = require( 'mongoose' );
-var sprovidersSchema = new mongoose.Schema({ });
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var db = mongoose.connection;
+
+var providerSchema = new Schema ({
+    name: {
+        type: String, 
+        required: true},
+    address: {
+        type: String, 
+        required: true},
+    rating: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 5},
+    attributes: {
+        type: String, 
+        required: true},
+    distance: {
+        type: String, 
+        required: true}
+});
